2c4462e3be909ca7e634800a00507c9c787238d1
Alexandre "kAworu" Perrin <kaworu(a)kaworu,ch>
2009-07-11 19:29:52 +0200

    Added 3-clauses BSD licence

--- mpd.lua
+++ mpd.lua
@@ -1,10 +1,47 @@
+--
+--  _                                       _
+-- | |_   _  __ _       _ __ ___  _ __   __| |
+-- | | | | |/ _` |_____| '_ ` _ \| '_ \ / _` |
+-- | | |_| | (_| |_____| | | | | | |_) | (_| |
+-- |_|\__,_|\__,_|     |_| |_| |_| .__/ \__,_|
+--                               |_|
+--
 -- Small interface to MusicPD
 -- use luasocket, with a persistant connection to the MPD server.
 --
--- Author: Alexandre "kAworu" Perrin <kaworu at kaworu dot ch>
---
 -- based on a netcat version from Steve Jothen <sjothen at gmail dot com>
 -- (see http://github.com/otkrove/ion3-config/tree/master/mpd.lua)
+--
+--
+-- Copyright (c) 2008-2009, Alexandre Perrin <kaworu@kaworu.ch>
+-- All rights reserved.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions
+-- are met:
+--
+-- 1. Redistributions of source code must retain the above copyright
+--    notice, this list of conditions and the following disclaimer.
+-- 2. Redistributions in binary form must reproduce the above copyright
+--    notice, this list of conditions and the following disclaimer in the
+--    documentation and/or other materials provided with the distribution.
+-- 4. Neither the name of the author nor the names of its contributors
+--    may be used to endorse or promote products derived from this software
+--    without specific prior written permission.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+-- ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+-- FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+-- DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+-- OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+-- HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+-- LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+-- OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+-- SUCH DAMAGE.
+
+
 require("socket")
 
 -- Grab env
