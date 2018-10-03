f95036c4bd5b2cadd0ea42769d30c1afd33bf39a
Doron Behar <doron.behar@gmail.com>
2018-10-03 20:25:54 +0300

    Use string.find only when there is a password
    Check if '@' is found in the hostname string taken from the environment.

--- mpd.lua
+++ mpd.lua
@@ -20,8 +20,12 @@ local MPD = {}
 local function get_params_from_env()
     local port = tonumber(os.getenv('MPD_PORT')) or 6600
     local host = os.getenv('MPD_HOST') or 'localhost'
-    local _, _, password, hostname = string.find(host, "([^@]+)@([a-zA-Z0-9.]+)")
-    local port = tonumber(os.getenv('MPD_PORT'))
+    local password, hostname
+    if string.find(host,"@") then
+        _, _, password, hostname = string.find(host, "([^@]+)@([a-zA-Z0-9.]+)")
+    else
+        hostname = host
+    end
     return {
         hostname = hostname,
         port = port,
